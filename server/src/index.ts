be5ad3c97fb988b4df3e77e43acab01379d9b4d2
Gautam-2604 <gaurisariag@gmail.com>
2024-08-15 21:38:18 +0530

    User routes created

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import morgan from "morgan";
 
 import dashboardRoutes from '../src/routes/dashboardRoutes'
 import productRoutes from "./routes/productRoutes";
+import userRoute from "./routes/userRoute";
 
 
 
@@ -24,6 +25,7 @@ app.use(cors());
 
 app.use('/dashboard', dashboardRoutes)
 app.use('/products', productRoutes)
+app.use('/user', userRoute)
 
 const port= process.env.PORT || 3002
 app.listen(port,()=>{
